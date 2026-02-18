70908b52dd47454af7a1705fba6ac0242abc7e63
Jessy Johnson <johnson.jess.ann@gmail.com>
2026-02-18 17:03:16 -0500

    Tests for forgot_password, api_v1_forgot_password, api_v2_forgot_password, and api_v3_forgot_password added

--- tests/test_sqli.py
+++ tests/test_sqli.py
@@ -109,3 +109,412 @@ def test_login_hardened_incorrect(client, setup_test_db):
 
     assert res.status_code == 401
     assert data["status"] == "error"
+
+
+# testing forgot_password()
+def test_forgot_password_vuln_inj(client, setup_test_db):
+    """
+    Tests if SQL injection allowed when vulnerable
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_password_vuln_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_password_vuln_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_password_hardened_inj(client, setup_test_db):
+    """
+    Tests if SQL injection is blocked on forgot_password when in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_password_hardened_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_password_hardened_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+# testing api_v1_forgot_password()
+def test_forgot_v1_password_vuln_inj(client, setup_test_db):
+    """
+    Tests if SQL injection allowed when vulnerable
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v1_password_vuln_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v1_password_vuln_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v1_password_hardened_inj(client, setup_test_db):
+    """
+    Tests if SQL injection is blocked on forgot_password when in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v1_password_hardened_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v1_password_hardened_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v1/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+# testing api_v2_forgot_password()
+def test_forgot_v2_password_vuln_inj(client, setup_test_db):
+    """
+    Tests if SQL injection allowed when vulnerable
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v2_password_vuln_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v2_password_vuln_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v2_password_hardened_inj(client, setup_test_db):
+    """
+    Tests if SQL injection is blocked on forgot_password when in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v2_password_hardened_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v2_password_hardened_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v2/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+# testing api_v3_forgot_password()
+def test_forgot_v3_password_vuln_inj(client, setup_test_db):
+    """
+    Tests if SQL injection allowed when vulnerable
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v3_password_vuln_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v3_password_vuln_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in vulnerable state.
+    """
+    toggle_harden(False)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v3_password_hardened_inj(client, setup_test_db):
+    """
+    Tests if SQL injection is blocked on forgot_password when in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "' OR '1'='1"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
+
+
+def test_forgot_v3_password_hardened_correct(client, setup_test_db):
+    """
+    Tests if a valid username is accepted in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "testuser1"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 200
+    assert data["status"] == "success"
+
+
+def test_forgot_v3_password_hardened_incorrect(client, setup_test_db):
+    """
+    Tests if a non-existent username is rejected in hardened state.
+    """
+    toggle_harden(True)
+
+    payload = {
+        "username": "nonexistentuser"
+    }
+
+    res = client.post("/api/v3/forgot-password", json=payload)
+    data = res.get_json()
+
+    assert res.status_code == 404
+    assert data["status"] == "error"
